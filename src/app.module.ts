6cc78619d911dcb06695bdf823807d5eb432bfbf
Kene <kayyungx@gmail.com>
2023-04-14 11:14:29 +0100

    Setup graphql and apollo sandbox

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
