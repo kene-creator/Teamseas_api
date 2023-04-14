5d6a1b994f98efe5949e73f4081e7830f051e324
Kene <kayyungx@gmail.com>
2023-04-14 14:08:51 +0100

    Setup donations schema and apollo service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DonationsModule } from './donations/donations.module';
+import { GraphQLDateTime } from 'graphql-iso-date';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       playground: false,
+      resolvers: { DateTime: GraphQLDateTime },
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
